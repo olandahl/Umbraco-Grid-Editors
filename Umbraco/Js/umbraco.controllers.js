1671b326f0310307aeefcf5281540028b89b96e7
Olof Landahl <olof.landahl@hiq.se>
2014-09-16 10:58:53 +0200

    Reversed commit

--- Umbraco/Js/umbraco.controllers.js
+++ Umbraco/Js/umbraco.controllers.js
@@ -4590,7 +4590,7 @@ angular.module("umbraco")
     	}, 200);
 });
 angular.module("umbraco")
-    .controller("Umbraco.PropertyEditors.Grid.TextStringController",
+    .controller("Umbraco.PropertyEditors.Grid.TextStringControllerr",
     function ($scope, $rootScope, $timeout, dialogService) {
         
         $scope.adjustSize = function(ev){
