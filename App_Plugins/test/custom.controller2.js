d40a8b53fb1de69b59737de28d1efd8a899cabcc
Olof Landahl <olof.landahl@hiq.se>
2014-09-16 11:44:47 +0200

    Added plugins

---
+++ App_Plugins/test/custom.controller2.js
@@ -0,0 +1,5 @@
+angular.module("umbraco")
+    .controller("Custom.PropertyEditors.Grid.TestController2",
+    function () {
+        console.log("Custom.PropertyEditors.Grid.TestController2");
+    });
---
+++ App_Plugins/test2/custom.controller3.js
@@ -0,0 +1,5 @@
+angular.module("umbraco")
+    .controller("Custom.PropertyEditors.Grid.TestController3",
+    function () {
+        console.log("Custom.PropertyEditors.Grid.TestController3");
+    });
